d42239f59be66d30ce9eac5a6cfbc235a88b4527
Nagalakshman Chirukuri <chirukurinagalakshman@outlook.com>
2019-09-27 22:07:37 +0100

    Spring boot with free marker

---
+++ src/main/java/com/techbrothers/techsolutions/springbootfreemarker/SpringBootFreeMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.techbrothers.techsolutions.springbootfreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreeMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreeMarkerApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/templates/user.ftl
@@ -0,0 +1,6 @@
+<!Doctype html>
+<html>
+<h1>Welcome to Tech Brothers - Tech Solutions</h1>
+<h2>user info is</h2>
+</html>
+
